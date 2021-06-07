23ab694acf089230ed2f6e2d4a75578dbbeb26c4
Caitlin Birenbaum <caitlin.birenbaum@outlook.com>
2021-06-07 15:38:35 -0500

    changed labeling for timeblock, added js code to show colors, and modified css for past, present and future

--- script.js
+++ script.js
@@ -2,37 +2,46 @@
 
 $("#currentDay").text(moment().format('ddd, MMM Do'));
 
-//currentTime to determine if the hour matches for color
-var currentTime = moment().format('H');
-console.log()
+function currentTime() {
+  var current = moment().hours()
+  var time = $('.time-block');
 
-// Use window.localStoirage to retrieve and store your data object
-const LS = JSON.parse(localStorage.scheduler || '{}'); // now an Object
+  //    console.log(current);
 
-const rows = document.getElementsByClassName("row");
-let currentHour = parseInt(moment().format('H'));
+  time.each(function () {
+    var hour = parseInt($(this).attr('id'))
 
-Array.from(rows).forEach(row => {
-  let
-    rowIdString = row.id,
-    rowHour;
-  if (rowIdString) {
-    rowHour = parseInt(rowIdString);
-  }
-  if (rowHour) {
-    // Compares row id to current hour and sets color accordingly
-    if (currentHour === rowHour) {
-      setColor(row, "red");
-    } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
-      setColor(row, "green");
-    } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
-      setColor(row, "lightgrey");
+  //  console.log(hour);
+
+    if (hour === current) {
+      $(this).children('.col-sm-10').attr('class', 'present col-sm-10 description')
+  //    console.log(('present' + hour + current))
+    } else if (current > hour) {
+      $(this).children('.col-sm-10').attr('class', 'past col-sm-10 description')
+  //    console.log(('past' + hour + current))
     } else {
-      setColor(row, "white");
+      $(this).children('.col-sm-10').attr('class', 'future col-sm-10 description')
+  //    console.log(('future' + hour + current))
     }
-  }
-});
-
-function setColor(element, color) {
-  element.style.backgroundColor = color;
+  })
 }
+currentTime()
+
+//save to local storage
+button.on('click', function (event) {
+  event.preventDefault();
+  var text = $(this).siblings('.col-sm-10').val().replace(/['"]+/g, '');
+  var parent = $(this).parent().attr('id');
+  localStorage.setItem(parent, JSON.stringify(text));
+
+})
+
+$('#9 textarea').val(localStorage.getItem('9').replace(/['"]+/g, ''));
+$('#10 textarea').val(localStorage.getItem('10').replace(/['"]+/g, ''));
+$('#11 textarea').val(localStorage.getItem('11').replace(/['"]+/g, ''));
+$('#12 textarea').val(localStorage.getItem('12').replace(/['"]+/g, ''));
+$('#13 textarea').val(localStorage.getItem('13').replace(/['"]+/g, ''));
+$('#14 textarea').val(localStorage.getItem('14').replace(/['"]+/g, ''));
+$('#15 textarea').val(localStorage.getItem('15').replace(/['"]+/g, ''));
+$('#16 textarea').val(localStorage.getItem('16').replace(/['"]+/g, ''));
+$('#17 textarea').val(localStorage.getItem('17')).replace(/['"]+/g, '');
